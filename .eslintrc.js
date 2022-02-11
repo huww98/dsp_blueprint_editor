1347ba75280c41e3dc2c06b6d1994ad3935bd7c2
胡玮文 <huww98@outlook.com>
2022-02-12 00:26:50 +0800

    vue add unit-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,16 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
