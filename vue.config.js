d49f9a85860e2b4cd15f1af9ae7702bda2ed750a
胡玮文 <huww98@outlook.com>
2022-02-18 00:58:23 +0800

    icons

--- vue.config.js
+++ vue.config.js
@@ -15,4 +15,34 @@ module.exports = defineConfig({
       ],
     }
   },
+  pwa: {
+    name: '戴森球计划蓝图预览',
+    themeColor: '#000000',
+    msTileColor: null,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      short_name: '蓝图预览',
+      icons: [
+        {
+          'src': './img/icons/android-chrome-192x192.png',
+          'sizes': '192x192',
+          'type': 'image/png'
+        },
+        {
+          'src': './img/icons/android-chrome-512x512.png',
+          'sizes': '512x512',
+          'type': 'image/png'
+        },
+      ],
+    },
+    iconPaths: {
+      faviconSVG: 'img/icons/favicon.svg',
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: null,
+      maskIcon: null,
+      msTileImage: null,
+    },
+  },
 })
