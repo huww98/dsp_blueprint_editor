b06b8194e25511812baa809564f69b45395a1951
胡玮文 <huww98@outlook.com>
2022-02-17 19:24:31 +0800

    publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/dsp_blueprint_editor/',
   transpileDependencies: false,
   configureWebpack: {
     module: {
