21313a8318697954c9dcfa1712f05657b5a94719
胡玮文 <huww98@outlook.com>
2022-05-21 16:00:09 +0800

    upgrade deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,10 @@
 module.exports = {
     root: true,
     env: {
-        node: true
+        node: true,
+        "vue/setup-compiler-macros": true,
     },
+    parser: 'vue-eslint-parser',
     'extends': [
         'plugin:vue/vue3-essential',
         'eslint:recommended',
